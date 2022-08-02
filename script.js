96d16115643f66032fcadf16f4e529209b02765d
campbell-scott <scott99campbell@gmail.com>
2022-08-02 20:57:04 -0300

    1.1

--- script.js
+++ script.js
@@ -9,46 +9,49 @@ let libros = [
     {ISBN: 9789502812441, Titulo: "El Club De Las 5 De La Mañana", Stock: 36},
     {ISBN: 9789874433428, Titulo: "De Comerciante a Empresario", Stock: 20},
 ]
-console.log("Opciones:")
-console.log("1. Ingresar un libro al catalogo")
-console.log("2. Crear una orden de compra")
-console.log("3. Ver catalogo y stock")
-console.log("4. Salir")
 
-let opcion = ""
-do{
-    opcion = parseInt(prompt("Ingrese una opcion"))
-    switch (opcion) {
-        case 1:
-            //ingresar un libro al catalogo
-            let cantLibros = parseInt(prompt("Ingrese la cantidad de libros que quiere agregar"))
-            for (let i = 1; i <= cantLibros; i++) {
-                let isbn = parseInt(prompt("Ingrese el ISBN del libro"))
-                let titulo = prompt("Ingrese el titulo del libro")
-                let stock = parseInt(prompt("Ingrese el stock del libro"))
-                libros.push({isbn, titulo, stock})
-            }
-            break;
-        case 2:
-            //crear una orden de compra
-            for (let i = 0; i < libros.length; i++) {
-                let ventas = parseInt(prompt("Ingrese las ventas de un mes de "+libros[i].Titulo))
-            if ((ventas*1.5)<=(libros[i].Stock)) {
-                console.log("De el libro "+libros[i].Titulo+" tiene que pedir 0 ejemplares")
-            }
-            else {
-                console.log("De el libro "+libros[i].Titulo+" tiene que pedir "+((ventas*1.5)-libros[i].Stock)+" ejemplares")
-            }
-            }
-            break;
-        case 3:
-            //Ver catalogo
-            libros.forEach(element => console.log(element))
-            console.log("El stock total es: " + totalStock())
-            break;
-        default:
-            break;
-    } 
-}while (opcion != 4)
+let titulo = document.getElementById('titulo')
+let isbn = document.getElementById('isbn')
+let stock = document.getElementById('stock')
+let input1 = document.getElementById('input1')
+let input2 = document.getElementById('input2')
+let input3 = document.getElementById('input3')
+let boton = document.getElementById('boton')
+boton.addEventListener('click', agregarLibro)
 
-console.log("Fin programa")
+function agregarLibro() {
+    let nombrePantalla = document.createElement('p')
+    nombrePantalla.innerHTML = input1.value
+    titulo.append(nombrePantalla)
+    let codigoPantalla = document.createElement('p')
+    codigoPantalla.innerHTML = input2.value
+    isbn.append(codigoPantalla)
+    let cantidadPantalla = document.createElement('p')
+    cantidadPantalla.innerHTML = input3.value
+    stock.append(cantidadPantalla)
+    let Titulo = input1.value
+    let ISBN = parseInt(input2.value)
+    let Stock = parseInt(input3.value)
+    libros.push({ISBN, Titulo, Stock})
+    console.log(libros)
+}
+
+let compra = document.getElementById('compra')
+let cargar = document.getElementById('cargar')
+cargar.addEventListener('click', dispararCompra)
+
+function dispararCompra() {
+    for (let i = 0; i < libros.length; i++) {
+        let ventas = parseInt(prompt("Ingrese las ventas de un mes de "+libros[i].Titulo))
+    if ((ventas*1.5)<=(libros[i].Stock)) {
+        let compraPantalla = document.createElement('p')
+        compraPantalla.innerHTML = "De el libro "+libros[i].Titulo+" tiene que pedir 0 ejemplares"
+        compra.append(compraPantalla)
+    }
+    else {
+        let compraPantalla = document.createElement('p')
+        compraPantalla.innerHTML = "De el libro "+libros[i].Titulo+" tiene que pedir "+Math.ceil(((ventas*1.5)-libros[i].Stock))+" ejemplares"
+        compra.append(compraPantalla)
+    }
+    }
+}
